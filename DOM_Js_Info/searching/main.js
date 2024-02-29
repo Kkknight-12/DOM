34fd97f8ead5a25cc7280111a5f191e272b9c717
Kkknight-12 <mayanks365@gmail.com>
2024-02-29 09:14:45 +0530

    Add searching notes

--- DOM_Js_Info/searching/main.js
+++ DOM_Js_Info/searching/main.js
@@ -1,21 +1,27 @@
 // https://javascript.info/searching-elements-dom#getelementsby
 
 // Searching: getElement*, querySelector*
-// document.getElementById or just id
-// if there are multiple elements with the same id, only the first one will be returned
-//
 
-let elem = document.getElementById("elem");
+// getElement
+
+// If an element has the id attribute, we can get the element using
+// document.getElementById(id). The id is unique within the document.
+// if there are multiple elements with the same id, only the first one will
+// be returned
+
+let elem = document.getElementById('elem')
 // make its background red
-elem.style.background = "red";
+elem.style.background = 'darkorange'
 
-let elems1 = document.getElementById("elem-content1");
-elems1.style.color = "white";
-console.log(elems1);
+let elems1 = document.getElementById('elem-content1')
+elems1.style.color = 'white'
+console.log('Element1 ', elems1) // <div id="elem-content1">...</div>
 
+// NOTE:
 // window is an object that represents the browser window
 // with window["elem-content"] we can access the element with id="elem-content"
-window["elem-content2"].style.background = "blue";
+window['elem-content2'].style.background = 'blue'
+window['elem-content2'].style.color = 'white'
 
 // but with window["elem-content"] we can't access class="elem-content"
 // window["elem-content3"].style.background = "blue";
@@ -31,14 +37,16 @@ window["elem-content2"].style.background = "blue";
  * on the window object for classes. Therefore, window["class-name"] will
  * not work to access elements by their class.
  *
- * To access elements by their class, you can use document.getElementsByClassName("class-name")
- * or document.querySelector(".class-name"). These methods return a live HTMLCollection
- * and the first matching Element respectively.
+ * To access elements by their class, you can use
+ * document.getElementsByClassName("class-name")
+ * or document.querySelector(".class-name"). These methods return a live
+ * HTMLCollection and the first matching Element respectively.
  * */
 
 // --------------------------------------------------------------------------------
 
 // querySelectorAll
+
 // document.querySelectorAll(css) returns all elements inside the document
 // that match the given CSS selector. The result is a static (not live) NodeList.
 
@@ -52,10 +60,10 @@ window["elem-content2"].style.background = "blue";
   <li>passed</li>
 </ul>
 * */
-let elements = document.querySelectorAll("ul > li:last-child");
+let elements = document.querySelectorAll('ul > li:last-child')
 
 for (let elem of elements) {
-    console.log(elem.innerHTML); // "test", "passed"
+  console.log(elem.innerHTML) // "test", "passed"
 }
 
 /*
@@ -81,9 +89,6 @@ for (let elem of elements) {
  * feed of the DOM that updates in real time.
  * */
 
-/*
- * */
-
 // --------------------------------------------------------------------------------
 
 // querySelector
@@ -101,10 +106,10 @@ for (let elem of elements) {
 <a href="http://ya.ru">...</a>
 * */
 for (let elem of document.body.children) {
-    // if it's an <a> elem with href ending in .zip
-    if (elem.matches('a[href$="zip"]')) {
-        console.log("The archive reference: " + elem.href);
-    }
+  // if it's an <a> elem with href ending with zip
+  if (elem.matches('a[href$="zip"]')) {
+    console.log('The archive reference: ' + elem.href)
+  }
 }
 
 // --------------------------------------------------------------------------------
@@ -121,38 +126,48 @@ for (let elem of document.body.children) {
 /*
 <h1>Contents</h1>
 
-<div class="contents">
-  <ul class="book">
-    <li class="chapter">Chapter 1</li>
+<div class="contents"> <!-- the closest ancestor -->
+  <ul class="book"> <!-- the closest ancestor -->
+    <li class="chapter">Chapter 1</li> <!-- the elem itself -->
     <li class="chapter">Chapter 2</li>
   </ul>
 </div>
 * */
 
 // <li class="chapter">Chapter 1</li>
-let chapter = document.querySelector(".chapter"); // LI
+let chapter = document.querySelector('.chapter') // LI
 
+console.log('closest .book ', chapter.closest('.book'))
 // <ul class="book"> ... </ul>
-console.log(chapter.closest(".book"));
+console.log('closest .contents ', chapter.closest('.contents'))
+// <div class="contents"> ... </div>
 
-// null (because it's not a descendant)
-console.log(chapter.closest("h1"));
+// null (because the h1 is not an ancestor)
+console.log('closest h1 ', chapter.closest('h1')) // null
 
 // --------------------------------------------------------------------------------
 
-// getElementsBy*
-// document.getElementsByClassName, document.getElementsByTagName, document.getElementsByName
+// getElementsBy* ( all methods have "s" )
+
+// document.getElementsByClassName,
+// document.getElementsByTagName,
+// document.getElementsByName
+
 // These methods are the oldest DOM methods. They are supported by all browsers.
-// They return a live HTMLCollection of elements.
+
+// They return a live - "HTMLCollection" of elements.
 
 /*
  * elem.getElementsByTagName(tag) looks for elements with the given tag and
- * returns the collection of them. The tag parameter can also be a star "*" for “any tags”.
+ * returns the collection of them. The tag parameter can also be a star "*"
+ * for “any tags”.
  *
- * elem.getElementsByClassName(className) returns elements that have the given CSS class.
+ * elem.getElementsByClassName(className) returns elements that have the
+ * given CSS class.
  *
- * document.getElementsByName(name) returns elements with the given name attribute,
- * document-wide. Very rarely used.
+ * document.getElementsByName(name) returns elements with the given name
+ * attribute, document-wide. Very rarely used.
+ *
  * */
 
 /*
@@ -176,11 +191,13 @@ console.log(chapter.closest("h1"));
 * */
 
 // getElementsByTagName - returns a collection of elements with the given tag name
-let inputs = table.getElementsByTagName("input");
-console.log(inputs); // HTMLCollection of 3 elements
+let inputs = table.getElementsByTagName('input')
+console.log(inputs)
+// [input, input, input, age: input]
+// HTMLCollection of 3 elements
 
 for (let input of inputs) {
-    console.log(input.value + ": " + input.checked);
+  console.log(input.value + ': ' + input.checked)
 }
 
 // getElementsByName - returns a collection of elements with the given name attribute
@@ -193,18 +210,71 @@ for (let input of inputs) {
 
 // find by name attribute
 // returns a collection of elements with the given name
-let form = document.getElementsByName("my-form");
-console.log(form); // HTMLCollection of 1 element
-console.log(form[0]); // <form name="my-form">...</form>
+let form = document.getElementsByName('my-form')
+console.log(form) // HTMLCollection of 1 element
+console.log(form[0]) // <form name="my-form">...</form>
 
 // getElementsByClassName
 // returns a collection of elements with the given class
 // form = form[0]
 // let articles = form.getElementsByClassName('article');
-let articles = document.getElementsByClassName("article");
-console.log(articles[0]); // <div class="article">Article</div>
-console.log(articles[1]); // <div class="long article">Long article</div>
+let articles = document.getElementsByClassName('article')
+console.log('articles ', articles) // HTMLCollection of 2 elements
+console.log(articles[0]) // <div class="article">Article</div>
+console.log(articles[1]) // <div class="long article">Long article</div>
 
 // --------------------------------------------------------------------------------
 
 // Live collections
+
+/*
+ * All methods "getElementsBy*" return a live collection. Such collections
+ * always reflect the current state of the document and “auto-update” when it
+ * changes.
+ *
+ * In the example below, there are two scripts.
+ *
+ * The first one creates a reference to the collection of <div>. As of now,
+ * its length is 1.
+ * The second scripts runs after the browser meets one more
+ * <div>, so its length is 2.
+ */
+
+let liveCol = document.getElementsByClassName('live-cl')
+const divColLength = liveCol[0].getElementsByTagName('div')
+
+console.log('live div collection  length ', divColLength.length) // 1
+
+const secondDiv = document.createElement('div')
+secondDiv.innerHTML = 'Live Second div'
+
+// appendChild
+
+// The appendChild method in JavaScript is used to add a new child node at
+// the end of all the children of a specified parent node.
+
+// let fchild = document.createElement('div')
+// fchild.innerHTML = 'fchild'
+//
+// secondDiv.appendChild(fchild)
+
+// Append to the first element in the collection
+liveCol[0].appendChild(secondDiv)
+
+console.log('live div collection  length ', divColLength.length) // 2
+
+// --------------------------------------------------------------------------------
+
+// Static collection
+// querySelectorAll returns a static collection. It’s like a fixed array of elements.
+
+const staticCol = document.querySelectorAll('.static-cl > div')
+console.log('staticCollection length ', staticCol.length) // 1
+
+const secondDiv2 = document.createElement('div')
+secondDiv2.innerHTML = 'Static Second div'
+
+// Append to the first element in the collection
+staticCol[0].appendChild(secondDiv2)
+
+console.log('staticCollection length ', staticCol.length) // 1
